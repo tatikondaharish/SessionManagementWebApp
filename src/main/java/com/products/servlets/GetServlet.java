19fc91198a7fcfb28aab615b2fd6cd77ddf0095f
HarishTatikonda <tatikondaharish.th@gmail.com>
2018-12-13 17:57:06 -0800

    Initial Commit

---
+++ src/main/java/com/products/servlets/GetServlet.java
@@ -0,0 +1,21 @@
+package com.products.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class GetServlet extends HttpServlet {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet (HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+		String response = "Welcome to Page";
+		resp.getWriter().println(response);
+	}
+}
